458d3aa3753337e287574558a56b287876dc5b0e
LucasGottardo <lukegottardo1602@gmail.com>
2025-03-06 12:22:45 -0300

    atualização2

---
+++ workspace/src/lucas/lucas/launcher.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            name='log_level',
+            default_value='INFO',
+            choices=
+['DEBUG', 'INFO','WARN','ERROR','FATAL'],
+            description='Flag to set log level'
+        ),
+
+        Node(
+            name='publisher',
+            package='lucas',
+            executable='publisher',
+            arguments=
+['--ros-args', '--log-level', LaunchConfiguration('log_level')]
+        ),
+        Node(
+            name='subscriber',
+            package='lucas',
+            executable='subscriber',
+            arguments=
+['--ros-args', '--log-level', LaunchConfiguration('log_level')]
+        ),
+    ])
