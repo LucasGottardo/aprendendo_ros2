2e2e864c7e188d78560b6a219f3edce1803a16a5
LucasGottardo <lukegottardo1602@gmail.com>
2025-04-09 16:47:49 -0300

    Update zap.py

--- workspace/obstaculo/obstaculo/zap.py
+++ workspace/obstaculo/obstaculo/zap.py
@@ -19,12 +19,14 @@ class MeuNo(Node):
 
     # Contrutor do nó
     def __init__(self):
-        # Aqui é definido o nome do nó
         super().__init__('zap')
         self.publisher = self.create_publisher(Twist,'/cmd_vel', 10)
         self.subscription = self.create_subscription(LaserScan,'/scan', self.listener_callback, 10)
         self.subscription = self.create_subscription(Odometry, '/odom', self.Odometry_callback, 10)
         self.objetivo = np.array([5.0,0.0])
+
+        self.desviando = False
+        self.tempo_inicio_desvio = None
 
     #manda as mensagens
     def listener_callback(self, msg):
@@ -49,41 +51,41 @@ class MeuNo(Node):
 
     def talker_callback(self):
         cmd = Twist()
-        cmd.linear.x = 0.2 #velocidade de linha reta
-        cmd.angular.z = 0.0 #velocidade angular
-
         destino = self.objetivo - np.array([self.x, self.y])
         distancia_final = np.linalg.norm(destino)
-
-        theta = np.arctan2(destino[1], destino[0]) #ajusta a angulação da direção que o robo deve seguir
-        ajustar_angulo = theta - self.yaw
-        ajustar_angulo = np.arctan2(np.sin(ajustar_angulo), np.cos(ajustar_angulo))
-
-        if abs(ajustar_angulo) > 0.1:
-            cmd.linear.x = 0.0
-            cmd.angular.z = 0.5
+        theta = np.arctan2(destino[1], destino[0])
+        ajustar_angulo = np.arctan2(np.sin(theta - self.yaw), np.cos(theta - self.yaw))
 
         if (distancia_final <= 0.2):
             cmd.linear.x = 0.0
             cmd.angular.z = 0.0
+            self.publisher.publish(cmd)
             print(distancia_final)
 
-
-        if (self.distancia_frente <= 1.0): #ajusta frontal
+        if not self.desviando and (self.distancia_frente <= 1.0 or self.distancia_direita <= 0.5 or self.distancia_esquerda <= 0.5):
+            self.desviando = True
+            self.tempo_inicio_desvio = time.time()
             cmd.linear.x = 0.0
             cmd.angular.z = 0.2
-            
-
-        if (self.distancia_direita <= 0.5): #ajusta direita
+            self.publisher.publish(cmd)
+            return
+    
+        if self.desviando:
+            if time.time() - self.tempo_inicio_desvio < 2:
+                cmd.linear.x = 0.0
+                cmd.angular.z = 0.5
+                self.publisher.publish(cmd)
+                return
+            else:
+                self.desviando = False
+        
+        if abs(ajustar_angulo) > 0.1:
             cmd.linear.x = 0.0
-            cmd.angular.z = 0.2
+            cmd.angular.z = 0.5
+        else:
+            cmd.linear.x = 0.2
+            cmd.angular.z = 0.0
             
-
-        if (self.distancia_esquerda <= 0.5): #ajusta esquerda
-            cmd.linear.x = 0.0
-            cmd.angular.z = 0.2
-            
-
         self.publisher.publish(cmd)
 
 
