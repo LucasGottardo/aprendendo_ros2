fe6db3cec839df342d21abb9b2a6d08abba1c112
LucasGottardo <lukegottardo1602@gmail.com>
2025-04-13 15:16:55 -0300

    a

--- workspace/src/wavefront/setup.py
+++ workspace/src/wavefront/setup.py
@@ -1,22 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import os
 from glob import glob
 
 package_name = 'wavefront'
 
-# Caminho absoluto para a pasta do pacote
-package_dir = os.path.abspath(os.path.dirname(__file__))
-
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Caminho corrigido para os arquivos do mapa
-        (os.path.join('share', package_name, 'maps'), 
-         glob(os.path.join(package_dir, 'maps', '*')))
+        (os.path.join('share', package_name, 'maps'), glob('maps/*.yaml') + glob('maps/*.pgm'))
     ],
     install_requires=['setuptools'],
     entry_points={
