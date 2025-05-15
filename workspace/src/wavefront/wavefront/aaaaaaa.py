7f2edf1b529c17892dfe4d33cd0c286d118281e6
LucasGottardo <lukegottardo1602@gmail.com>
2025-05-15 14:59:23 -0300

    Update aaaaaaa.py

--- workspace/src/wavefront/wavefront/aaaaaaa.py
+++ workspace/src/wavefront/wavefront/aaaaaaa.py
@@ -1,11 +1,18 @@
 import numpy as np
 
 
-class fofinho(node):
+class fofinho(Node):
     def __init__(self):
         super().__init__("publisher")
         self.publisher = self.create_publisher()
         self.subscription = self.create_subscription()
-            
-    def listerner_callback(self, msg):
         self.objetivo = np.array[10.00, 7.00]
+        self.inicio = [0.0, 0.0]
+
+        
+
+
+
+        
+
+        
